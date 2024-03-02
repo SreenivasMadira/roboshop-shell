f2e9bb77f5d1778f5489c8a34e52554a3d3246d9
msree <msreeni2656@gmail.com>
2024-03-02 12:35:20 +0530

    ansible

--- cart.sh
+++ cart.sh
@@ -1,17 +1,4 @@
-cp cart.service/etc/systemd/system/cart.service
+component=cart
+source common.sh
 
-dnf install nodejs -y
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
-npm install
-
-
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
-
+func_nodejs
--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo INput Password Missing
+  exit 1
+fi
 cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf module disable mysql -y
-dnf install mysql-community-server -y
+yum module disable mysql -y
+yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+systemctl start mysqld
+mysql_secure_installation --set-root-pass ${mysql_root_password}
